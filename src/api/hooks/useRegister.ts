2be753f7304bb3c3d6b67581f084d3a166ea1df7
Hassaan_98 <hassaanxd8@gmail.com>
2026-02-04 11:39:08 +0500

    Delete useRegister.ts

--- src/api/hooks/useRegister.ts
+++
@@ -1,7 +0,0 @@
-import { useMutation } from "@tanstack/react-query";
-import { register, type RegisterRequest, type LoginResponse } from "../auth";
-
-export const useRegister = () =>
-  useMutation<LoginResponse, Error, RegisterRequest>({
-    mutationFn: register,
-  });
