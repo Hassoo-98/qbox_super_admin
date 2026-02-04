cc24b3a97fb00af9f5e9701fd7e1bfb8a1812914
Hassaan_98 <hassaanxd8@gmail.com>
2026-02-04 11:39:38 +0500

    Create auth.service.ts

---
+++ src/services/auth.service.ts
@@ -0,0 +1,19 @@
+import api from "../lib/axios";
+import { normalizeApiError } from "../lib/apiError";
+
+export const AuthService={
+    login:async(
+        payload:{
+            email:string;
+            password:string
+        }
+    ):Promise<any>=>{
+        try{
+        const {data}=await api.post("/auth/login/",payload)
+        return data
+
+        }catch(error){
+         normalizeApiError(error)
+        }
+    }
+}
