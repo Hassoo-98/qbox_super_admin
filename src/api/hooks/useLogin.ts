3f3d90aae20a912ced441f91e5ac135d06693b70
Hassaan_98 <hassaanxd8@gmail.com>
2026-02-04 11:39:06 +0500

    Delete useLogin.ts

--- src/api/hooks/useLogin.ts
+++
@@ -1,7 +0,0 @@
-import { useMutation } from "@tanstack/react-query";
-import { login, type LoginRequest, type LoginResponse } from "../auth";
-
-export const useLogin = () =>
-  useMutation<LoginResponse, Error, LoginRequest>({
-    mutationFn: login,
-  });
