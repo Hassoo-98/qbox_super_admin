43610f2e8ad6c86f575a3ddfe8a1be46676089cc
Hassaan_98 <hassaanxd8@gmail.com>
2026-02-09 10:59:56 +0500

    Update driver.service.ts

--- src/services/driver.service.ts
+++ src/services/driver.service.ts
@@ -1,116 +1,117 @@
 import api from "../lib/axios";
 import { normalizeApiError } from "../lib/apiError";
-interface QueryParams{
-    search:string;
-    is_active:boolean;
-    page:number;
-    limit:number
+interface QueryParams {
+    search: string;
+    is_active: boolean;
+    page: number;
+    limit: number
 }
-interface DriverCreatePayload{
-    image:string;
-    driver_name:string;
-    phone_number:string;
-    email:string;
-    is_active:boolean;
+interface Driver {
+    id: string;
+    image: string;
+    driver_name: string;
+    phone_number: string;
+    email: string;
+    is_active: boolean;
 }
-interface Driver{
-    id:string;
-    image:string;
-    driver_name:string;
-    phone_number:string;
-    email:string;
-    is_active:boolean;
-    total_deliveries?:string;
-    success_rate?:string;
+
+interface DriverListData {
+    item: Driver[];
+    total: number;
+    page: number;
+    limit: number;
+    hasMore: boolean;
 }
-interface DriverListResponse{
-    success:boolean;
-    statusCode:number;
-    data:{
-        items:Driver[]
-    };
-    total:number;
-    page:number;
-    message:string;
-    limit:number;
-    hasMore:boolean;
+
+interface GetlistDriverResponse {
+    success: boolean;
+    statusCode: number;
+    message: string;
+    data: DriverListData;
 }
-export const DriverService={
-    getAll:async(
-     params:QueryParams
-    ):Promise<DriverListResponse | undefined>=>{
-        try{
-         const {data}=await api.get("/drivers/", { params })
-         return data
-        }catch(error){
-         normalizeApiError(error)
-         return undefined
-        }
-    },
-    create:async(
-        payload:DriverCreatePayload
-    ):Promise<DriverListResponse | undefined>=>{
-        try{
-            const {data}=await api.post("/driver/create",payload)
+
+interface CrudDriverResponse {
+    success: boolean;
+    statusCode: number;
+    message: string;
+    data: Driver;
+}
+export const DriverService = {
+    getAllDriver: async (
+        params: QueryParams
+    ): Promise<GetlistDriverResponse> => {
+        try {
+            const { data } = await api.get("/drivers/", { params })
             return data
-        }catch(error){
-            normalizeApiError(error)
-            return undefined
+        } catch (error) {
+            normalizeApiError(error);
+            throw error;
         }
     },
-   getSingle:async(
-    id:string
-   ):Promise<DriverListResponse | undefined>=>{
-    try{
-        const {data}=await api.get(`/driver/${id}`)
-        return data
+    createDriver: async (
+        payload: Driver
+    ): Promise<CrudDriverResponse> => {
+        try {
+            const { data } = await api.post("/driver/create", payload)
+            return data
+        } catch (error) {
+            normalizeApiError(error);
+            throw error;
+        }
+    },
+    getSingleDriver: async (
+        id: string
+    ): Promise<CrudDriverResponse> => {
+        try {
+            const { data } = await api.get(`/driver/${id}`)
+            return data
 
-    }catch(error){
-    normalizeApiError(error)
-    return undefined
-    }
-   },
-   changeStatus:async(
-    id:string,
-    payload:{
-        is_active:boolean
-    }
-   ):Promise<DriverListResponse | undefined>=>{
-    try{
-    const {data}=await api.patch(`/driver/${id}/change-status`,payload)
-    return data
+        } catch (error) {
+            normalizeApiError(error);
+            throw error;
+        }
+    },
+    changeDriverStatus: async (
+        id: string,
+        payload: {
+            is_active: boolean
+        }
+    ): Promise<CrudDriverResponse> => {
+        try {
+            const { data } = await api.patch(`/driver/${id}/change-status`, payload)
+            return data
 
-    }catch(error){
-    normalizeApiError(error)
-    return undefined
-    }
-   },
-   deleteDriver:async(
-    id:string
+        } catch (error) {
+            normalizeApiError(error);
+            throw error;
+        }
+    },
+    deleteDriver: async (
+        id: string
 
-   ):Promise<DriverListResponse | undefined>=>{
-    try{
-    const {data}=await api.delete(`/driver/${id}/delete`)
-    return data
+    ): Promise<CrudDriverResponse> => {
+        try {
+            const { data } = await api.delete(`/driver/${id}/delete`)
+            return data
 
-    }catch(error){
-     normalizeApiError(error)
-     return undefined
-    }
-   },
-   update:async(
-    id:string,
-    payload:Driver | Partial<Driver>
-   ):Promise<DriverListResponse | undefined>=>{
-    try{
-     const {data}=await api.patch(`/driver/${id}/update`,payload)
-     return data
+        } catch (error) {
+            normalizeApiError(error);
+            throw error;
+        }
+    },
+    updateDriver: async (
+        id: string,
+        payload: Driver | Partial<Driver>
+    ): Promise<CrudDriverResponse> => {
+        try {
+            const { data } = await api.patch(`/driver/${id}/update`, payload)
+            return data
 
-    }catch(error){
-    normalizeApiError(error)
-    return undefined
+        } catch (error) {
+            normalizeApiError(error);
+            throw error;
+        }
     }
-   }
 
 
 }
