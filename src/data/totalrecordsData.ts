0d4ea79c691b5e478ed92591fb36d2b30c5f3a69
fahad-se <chfahadrashid4@gmail.com>
2026-02-19 14:23:16 +0500

    fixing the ui

--- src/data/totalrecordsData.ts
+++ src/data/totalrecordsData.ts
@@ -7,7 +7,7 @@ export interface CardItem {
 
 export const getTotalrecordsData = (t: (key: string) => string) => [
   { id: 1, icon: '/assets/icons/dollar.png', title: '3000', subtitle: t('Total Revenue') },
-  { id: 2, icon: '/assets/icons/dollar.png', title: '3000', subtitle: t('Active QBoxes') },
+  { id: 2, icon: '/assets/icons/dollar.png', title: '3000', subtitle: t('Total QBoxes') },
   { id: 3, icon: '/assets/icons/dollar.png', title: '3000', subtitle: t('Total Service Providers') },
   { id: 4, icon: '/assets/icons/dollar.png', title: '3000', subtitle: t('Total Deliveries') },
 ];
